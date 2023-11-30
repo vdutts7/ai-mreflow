ee475114dff7c2a4c501e08cc3dd2d406a43ea8b
ViveK Dutta <vivekd@berkeley.edu>
2023-11-30 13:38:20 -0800

    fixed width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,11 @@ const Home = () => {
           Note: bot comments are not real and not associated with Matt Wolfe. This is fiction. Check out <a href="https://ai-mkbhd.vercel.app/" style={{opacity: 0.8 }} className="text-purple-500 hover:font-bold">MKBHD AI</a>
          </i>
       </div>
-      <div className="mx-auto mt-2 flex w-full md:ml-16 max-w-7xl flex-col md:flex-row dark2-gradient">
+      <div className="mx-auto mt-10 flex w-full max-w-7xl flex-col md:flex-row ">
         <div className="flex w-full md:ml-8 flex-row md:w-1/3 md:flex-col">
           <MREFLOW />
         </div>
-        <div className="w-full md:w-6/3">
+        <div className="w-full md:w-full md:mr-8">
           <Chat />
         </div>
       </div>
